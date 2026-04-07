5a1323c98008b7c59fc4338017dec92360750738
Wanessa Gonçalves <gwanessags@gmail.com>
2026-04-07 09:07:38 -0300

    adicionando homecontroller

---
+++ src/main/java/senac/tsi/games/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package senac.tsi.games.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "API Games rodando!";
+    }
+}
